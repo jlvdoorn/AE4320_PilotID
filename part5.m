60d2be784e08ebe6b92abe4aed9182685f2894c0
Jan van Doorn <jlvdoorn@outlook.com>
2022-05-12 16:00:25 +0200

    (almost) finalised all the code

--- part5.m
+++ part5.m
@@ -12,10 +12,11 @@ fs = 1/dt;
 freq  = f0*(1:N/2);
 omega = freq*2*pi;
 
+%% Part 5 - Model Validation
 ev = validation.e;
 uv = validation.u;
 
-%% Part 5 - Model Validation
+%% FRF of validation set
 ev_dft  = fft(ev,N);
 uv_dft  = fft(uv,N);
 
@@ -36,3 +37,21 @@ semilogx(ft_pkslocs*f0, phaHp,'*g');
 xlim([5e-2 5e0]); ylim([-450 90]); grid on;
 xlabel('\omega (rad/s)'); ylabel('\angle Hp(j\omega) (deg)')
 legend('Real H_{p}(j\omega)','Estimated H_{p}(j\omega)');
+
+%% Cost Function of Model A-D based on real Hp
+omega = ft_pkslocs*f0;
+Hpv = magHpv;
+
+for k=1:10
+    J_Av = @(x) sum(abs(Hpv(k)-x(1)*exp(-1i*omega(k)*x(2))*(x(3)^2)/((1i*omega(k)^2)+2*x(3)*x(4)*1i*omega(k)+x(3)^2))^2);
+    J_Bv = @(x) sum(abs(Hpv(k)-x(1)*(x(2)*1i*omega(k)+1)*exp(-1i*omega(k)*x(3))*(x(4)^2)/((1i*omega(k)^2)+2*x(4)*x(5)*1i*omega(k)+x(4)^2))^2);
+    J_Cv = @(x) sum(abs(Hpv(k)-x(1)*(x(2)*1i*omega(k)+1)/(x(3)*1i*omega(k)+1)*exp(-1i*omega(k)*x(4))*(x(5)^2)/((1i*omega(k)^2)+2*x(5)*x(6)*1i*omega(k)+x(5)^2))^2);
+    J_Dv = @(x) sum(abs(Hpv(k)-x(1)/(x(2)*1i*omega(k)+1)*exp(-1i*omega(k)*x(3))*(x(4)^2)/((1i*omega(k)^2)+2*x(4)*x(5)*1i*omega(k)+x(4)^2))^2);
+end
+
+Cost_Av = J_Av(A_opt);
+Cost_Bv = J_Bv(B_opt);
+Cost_Cv = J_Cv(C_opt);
+Cost_Dv = J_Dv(D_opt);
+
+save('part5_done.mat')
